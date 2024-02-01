17eee09a3ce589100a6aeabc52f5bcf70ef063f4
PierreDesbruns <pierre.desbruns@gmail.com>
2024-02-01 12:08:18 +0100

    Basic color handling

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 from PIL import Image, ImageTk
 import tkinter as tk
 from complexFractal import *
+import matplotlib.cm
 
-mandelbrot_set = MandelbrotSet(max_iterations=20)
 
-image = Image.new(mode="L", size=(720, 512))
-for pixel in Viewport(image, center=-0.75, zoom=200):
-    z_0 = complex(pixel)
-    pixel.color = int(255 - mandelbrot_set.stability(z_0) * 255)
+def denormalize(palette):
+    return [
+        tuple(int(channel * 255) for channel in color)
+        for color in palette
+    ]
+
+
+fractal = MandelbrotSet(max_iterations=256)
+
+image = Image.new(mode="RGB", size=(720, 512))
+
+colormap = matplotlib.cm.get_cmap("twilight").colors
+palette = denormalize(colormap)
+
+for pixel in Viewport(image, center=-0.7435 + 0.1314j, zoom=200000):
+    stability = fractal.stability(complex(pixel))
+    index = int(min(stability * len(palette), len(palette) - 1))
+    pixel.color = palette[index % len(palette)]
 
 root = tk.Tk()
 image = ImageTk.PhotoImage(image)
